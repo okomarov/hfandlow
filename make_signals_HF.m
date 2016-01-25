174735ccd8e25d67ef9facf5173dfb4f709524bd
Oleg Komarov <o.komarov11@imperial.ac.uk>
2016-01-25 23:39:22 +0000

    add quarterly and semiannual

--- make_signals_HF.m
+++ make_signals_HF.m
@@ -1,23 +1,39 @@
-function signals = make_signals_HF(permno,date,beta)
+function signals = make_signals_HF(permno,date,betacomp)
 
 [unDt,~,midx] = unique(date/100);
 nmonths       = numel(unDt);
 nseries       = numel(permno);
-signals       = NaN(nmonths, nseries,2);
+signals       = NaN(nmonths, nseries,4);
 
 % Monthly
 for ii = 1:nmonths
-    imonth          = midx == ii;
-    tmp             = squeeze(nansum(beta(imonth,:,:)));
+    idx             = midx == ii;
+    tmp             = squeeze(nansum(betacomp(idx,:,:)));
     betas           = tmp(:,1)./tmp(:,2);
     signals(ii,:,1) = 0.5*betas + 0.5*nanmean(betas);
 end
 
-% Yearly
-for ii = 12:nmonths
-    iyear           = ismember(midx, ii-12+1:ii);
-    tmp             = squeeze(nansum(beta(iyear,:,:)));
+% quarterly
+for ii = 4:nmonths
+    idx             = ismember(midx, ii-4+1:ii);
+    tmp             = squeeze(nansum(betacomp(idx,:,:)));
     betas           = tmp(:,1)./tmp(:,2);
     signals(ii,:,2) = 0.5*betas + 0.5*nanmean(betas);
+end
+
+% semi-annually
+for ii = 6:nmonths
+    idx             = ismember(midx, ii-6+1:ii);
+    tmp             = squeeze(nansum(betacomp(idx,:,:)));
+    betas           = tmp(:,1)./tmp(:,2);
+    signals(ii,:,3) = 0.5*betas + 0.5*nanmean(betas);
+end
+
+% Yearly
+for ii = 12:nmonths
+    idx             = ismember(midx, ii-12+1:ii);
+    tmp             = squeeze(nansum(betacomp(idx,:,:)));
+    betas           = tmp(:,1)./tmp(:,2);
+    signals(ii,:,4) = 0.5*betas + 0.5*nanmean(betas);
 end
 end
