dc87dac4df12a27dfe15c966c176d127e5326216
Oleg Komarov <o.komarov11@imperial.ac.uk>
2016-01-26 00:44:59 +0000

    allow shrinkage w and reorganize

--- make_signals_HF.m
+++ make_signals_HF.m
@@ -1,39 +1,24 @@
-function signals = make_signals_HF(permno,date,betacomp)
+function signals = make_signals_HF(permno,date,betacomp,w)
 
 [unDt,~,midx] = unique(date/100);
 nmonths       = numel(unDt);
 nseries       = numel(permno);
 signals       = NaN(nmonths, nseries,4);
 
-% Monthly
-for ii = 1:nmonths
-    idx             = midx == ii;
-    tmp             = squeeze(nansum(betacomp(idx,:,:)));
-    betas           = tmp(:,1)./tmp(:,2);
-    signals(ii,:,1) = 0.5*betas + 0.5*nanmean(betas);
+signals(:,:,1) = getBetaHF(nmonths,nseries,betacomp,midx,1,w);
+signals(:,:,2) = getBetaHF(nmonths,nseries,betacomp,midx,4,w);
+signals(:,:,3) = getBetaHF(nmonths,nseries,betacomp,midx,6,w);
+signals(:,:,4) = getBetaHF(nmonths,nseries,betacomp,midx,12,w);
 end
 
-% quarterly
-for ii = 4:nmonths
-    idx             = ismember(midx, ii-4+1:ii);
-    tmp             = squeeze(nansum(betacomp(idx,:,:)));
-    betas           = tmp(:,1)./tmp(:,2);
-    signals(ii,:,2) = 0.5*betas + 0.5*nanmean(betas);
+function betas = getBetaHF(nmonths,nseries,betacomp,midx,len,w)
+betas = NaN(nmonths, nseries);
+for ii = len:nmonths
+    idx         = ismember(midx, ii-len+1:ii);
+    tmp         = squeeze(nansum(betacomp(idx,:,:)));
+    betas(ii,:) = tmp(:,1)./tmp(:,2);
 end
-
-% semi-annually
-for ii = 6:nmonths
-    idx             = ismember(midx, ii-6+1:ii);
-    tmp             = squeeze(nansum(betacomp(idx,:,:)));
-    betas           = tmp(:,1)./tmp(:,2);
-    signals(ii,:,3) = 0.5*betas + 0.5*nanmean(betas);
-end
-
-% Yearly
-for ii = 12:nmonths
-    idx             = ismember(midx, ii-12+1:ii);
-    tmp             = squeeze(nansum(betacomp(idx,:,:)));
-    betas           = tmp(:,1)./tmp(:,2);
-    signals(ii,:,4) = 0.5*betas + 0.5*nanmean(betas);
+if w ~= 1
+    betas = w*betas + (1-w)*nanmean(betas,2);
 end
 end
