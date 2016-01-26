01c87ef6566067f88866085e8cabfac42acd99ae
Oleg Komarov <o.komarov11@imperial.ac.uk>
2016-01-26 01:09:56 +0000

    encapsulate

--- make_signals_LF.m
+++ make_signals_LF.m
@@ -1,11 +1,11 @@
 function [signals,hpr,rf,mdate] = make_signals_LF(ret,date,factors,w,lag)
-if nargin < 5 || isempty(lag)
-    lag = 0;
-end
 if nargin < 4
     isBayesW = true;
 else
     isBayesW = false;
+end
+if nargin < 5 || isempty(lag)
+    lag = 0;
 end
 
 [mdate,~,midx] = unique(date/100);
@@ -18,24 +18,9 @@ rf      = NaN(nmonths, 1);
 
 % Monthly
 for ii = 1:nmonths
-    idx   = midx == ii;
-    r     = ret(idx,:);
-    ff    = factors(idx,:);
-    betas = NaN(lag,size(r,2));
-    n     = nnz(idx);
-    
-    % Dimson betas if lag > 1
-    for jj = 0:lag-1
-        [betas(jj+1,:),sigma_ts] = getBetasLF(r(1+jj:n,:),ff(1:n-jj,:),n-jj,isBayesW);
-    end
-    betas = sum(betas,1);
-    
-    if isBayesW
-        sigma_xs = nanvar(betas,2);
-        w        = 1-sigma_ts./(sigma_ts+sigma_xs);
-    end
-    signals(ii,:,1) = w.*betas + (1-w)*nanmean(betas,2);
-    
+    idx             = midx == ii;
+    signals(ii,:,1) = getBetasLF(ret, factors, idx, lag, w, isBayesW);
+
     % HPR
     r                 = ret(idx,:);
     inan              = isnan(r);
@@ -49,21 +34,46 @@ end
 % Quarterly
 for ii = 4:nmonths
     idx             = ismember(midx, ii-4+1:ii);
-    signals(ii,:,2) = getBetasLF(ret(idx,:),factors(idx,:),50,isBayesW);
+    signals(ii,:,2) = getBetasLF(ret, factors, idx, lag, w, isBayesW, 50);
 end
 % Semi-annual
 for ii = 6:nmonths
     idx             = ismember(midx, ii-6+1:ii);
-    signals(ii,:,3) = getBetasLF(ret(idx,:),factors(idx,:),100,isBayesW);
+    signals(ii,:,3) = getBetasLF(ret, factors, idx, lag, w, isBayesW, 100);
 end
 % Annual
 for ii = 12:nmonths
     idx             = ismember(midx, ii-12+1:ii);
-    signals(ii,:,4) = getBetasLF(ret(idx,:),factors(idx,:),200,isBayesW);
+    signals(ii,:,4) = getBetasLF(ret, factors, idx, lag, w, isBayesW, 200);
 end
 end
 
-function [betas,sigma_ts] = getBetasLF(r,factors,minobs,isBayesW)
+function betas = getBetasLF(ret, factors, idx, lag, w, isBayesW, minobs)
+r     = ret(idx,:);
+ff    = factors(idx,:);
+betas = NaN(1+lag,size(r,2));
+n     = size(r,1);
+
+% Dimson betas if lag > 1
+for jj = 0:lag
+    if nargin < 7
+        minobs = n-jj;
+    end
+    [betas(jj+1,:),sigma_ts] = getBetasLF_(r(1+jj:n,:),ff(1:n-jj,:),minobs,isBayesW);
+end
+betas = sum(betas,1);
+
+if isBayesW
+    sigma_xs = nanvar(betas,2);
+    w        = 1-sigma_ts./(sigma_ts+sigma_xs);
+end
+
+if w ~= 1
+    betas = w.*betas + (1-w)*nanmean(betas,2);
+end
+end
+
+function [betas,sigma_ts] = getBetasLF_(r,factors,minobs,isBayesW)
 betas = NaN(1,size(r,2));
 
 % At least x non NaN returns
