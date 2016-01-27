ab2971b61d82de4b9ef33fb01a09863df5e6cb6e
Oleg Komarov <o.komarov11@imperial.ac.uk>
2016-01-27 18:02:15 +0000

    cosmetic

--- main.m
+++ main.m
@@ -44,21 +44,18 @@ for ii = 1:nsig
     [ptfret{ii,2},~,~,~,avgsig{ii,2}] = bab(hpr,signals_HF(:,:,ii),rf);
 end
 %% Desc stats
+
+% Correlation
 snames       = {'babm','babq','babs','baby','rbabm','rbabq','rbabs','rbaby'};
 order        = reshape(reshape(1:nsig*2,nsig,2)',1,nsig*2);
 allsig       = cat(3,signals_LF,signals_HF);
 correlations = corrxs(allsig(:,:,order),snames(order));
 
+% Plot
 dt   = serial2datetime(datenum(1993,(1:size(hpr,1))+2,1)-1);
 idec = dt >= yyyymmdd2datetime(20010501);
-
-desc   = cellfun(@(x) stratstats(dt,x*100,'Frequency','m','IsPercentageReturn',true), ptfret,'un',0);
-desc   = cellfun(@(x) renameVarNames(x',{'Low','High','BAB'}), desc,'un',0);
-catfun = @(sig,stats) [stats; array2table(nanmean(sig),'VariableNames',{'Low','High','BAB'},'RowNames',{'Avgsig'})];
-desc   = cellfun(catfun,avgsig,desc,'un',0);
-
 figure
-X = datenum([min(dt(idec)), min(dt(idec)), max(dt(idec)),max(dt(idec))]);
+X    = datenum([min(dt(idec)), min(dt(idec)), max(dt(idec)),max(dt(idec))]);
 YLIM = [0,6];
 for ii = 1:nsig*2
     subplot(nsig,2,ii)
@@ -71,15 +68,20 @@ for ii = 1:nsig*2
     set(gca, 'TickLabelInterpreter','latex','Ylim',YLIM,'YTick',0:2:YLIM(2),'Layer','Top')
 end
 
-%% PTFRET after decimalization
-% Jan 2001 decimalization in NYSE and April in NASDAQ, take from May
+% Desc stats
+desc   = cellfun(@(x) stratstats(dt,x*100,'Frequency','m','IsPercentageReturn',true), ptfret,'un',0);
+desc   = cellfun(@(x) renameVarNames(x',{'Low','High','BAB'}), desc,'un',0);
+catfun = @(sig,stats) [stats; array2table(nanmean(sig),'VariableNames',{'Low','High','BAB'},'RowNames',{'Avgsig'})];
+desc   = cellfun(catfun,avgsig,desc,'un',0);
+out = cell2mat(cellfun(@(x) x{:,:}, desc,'un',0));
 
+% Desc stats, post decimalization
+% Jan 2001 decimalization in NYSE and April in NASDAQ, take from May
 desc2  = cellfun(@(x) stratstats(dt(idec),x(idec,:)*100,'Frequency','m','IsPercentageReturn',true), ptfret,'un',0);
 desc2  = cellfun(@(x) renameVarNames(x',{'Low','High','BAB'}), desc2,'un',0);
 catfun = @(sig,stats) [stats; array2table(nanmean(sig(idec,:)),'VariableNames',{'Low','High','BAB'},'RowNames',{'Avgsig'})];
 desc2  = cellfun(catfun,avgsig,desc2,'un',0);
-
-
+out = cell2mat(cellfun(@(x) x{:,:}, desc2,'un',0));
 %% Tests
 [coeff,se,tratio,pval] = regressHighOnLow(ptfret(:,2),ptfret(:,1));
 [coeff2,se2,tratio2,pval2] = regressHighOnLow(ptfret(:,2),ptfret(:,1),idec);
