0869ca56cc6b86d174ebd99fce63c121e9cbf1d6
Oleg Komarov <o.komarov11@imperial.ac.uk>
2016-02-02 19:25:02 +0000

    add correlation after decimalization

--- main.m
+++ main.m
@@ -1,5 +1,5 @@
 %% Options
-OPT_USEETF = true;
+OPT_USEETF = false;
 
 OPT_LAG    = 1;
 OPT_PTF_UN = 5;
@@ -58,16 +58,17 @@ for ii = 1:nsig
     [ptfret{ii,2},~,~,~,avgsig{ii,2}] = bab(hpr,signals_HF(:,:,ii),rf);
 end
 %% Desc stats
+dt   = serial2datetime(datenum(1993,(1:size(signals_LF,1))+2,1)-1);
+idec = dt >= yyyymmdd2datetime(20010501);
 
 % Correlation
 snames       = {'babm','babq','babs','baby','rbabm','rbabq','rbabs','rbaby'};
 order        = reshape(reshape(1:nsig*2,nsig,2)',1,nsig*2);
 allsig       = cat(3,signals_LF,signals_HF);
 correlations = corrxs(allsig(:,:,order),snames(order));
+correlations_dec = corrxs(allsig(idec,:,order),snames(order));
 
 % Plot
-dt   = serial2datetime(datenum(1993,(1:size(hpr,1))+2,1)-1);
-idec = dt >= yyyymmdd2datetime(20010501);
 figure
 X    = datenum([min(dt(idec)), min(dt(idec)), max(dt(idec)),max(dt(idec))]);
 YLIM = [0,6];
@@ -105,104 +106,3 @@ out = cell2mat(cellfun(@(x) x{:,:}, desc2,'un',0));
 %% Risk-adjustment
 factors = loadresults('RAfactors');
 factors = factors(ismember(factors.Date, mdate),:);
-%% RV, RCOV, RBETA plots
-
-% Alcoa
-aa          = getTaqData('symbol','AA',[],[],[],'..\data\TAQ\sampled\5min\nobad_vw');
-% issorted(aa.Datetime)
-aa.Ret      = [NaN; diff(log(aa.Price))];
-ion         = [true; diff(fix(aa.Datetime))~=0];
-aa.Ret(ion) = NaN;
-
-spy          = getSpy(5);
-spy.Ret      = [NaN; diff(log(spy.Price))];
-ion          = [true; diff(fix(spy.Datetime))~=0];
-spy.Ret(ion) = NaN;
-
-% Intersect dates
-Date = intersect(fix(aa.Datetime), fix(spy.Datetime));
-aa   = aa(ismember(fix(aa.Datetime), Date),:);
-spy  = spy(ismember(fix(spy.Datetime), Date),:);
-
-% NO OVERNIGHT
-%%%%%%%%%%%%%%
-rv   = @(subs) sqrt(accumarray(subs,spy.Ret.^2,[],@nansum));
-rcov = @(subs,stock) accumarray(subs,spy.Ret.*stock.Ret,[],@nansum);
-
-% Daily
-[Date,~,iday] = unique(serial2yyyymmdd(spy.Datetime));
-RVd           = rv(iday);
-RCVd          = rcov(iday,aa);
-
-% Monthly
-[~,pos,imonth] = unique(serial2yyyymmdd(spy.Datetime)/100,'last');
-RVm            = rv(imonth);
-RCVm           = rcov(imonth,aa);
-
-% Yearly, rolling every month
-RVy      = NaN(size(RCVm));
-RCVy     = NaN(size(RCVm));
-rcov_run = @(idx,stock) nansum(spy.Ret(idx).*stock.Ret(idx));
-for ii = 12:size(RCVm,1)
-    idx      = ismember(imonth, ii-11:ii);
-    RVy(ii)  = sqrt(nansum(spy.Ret(idx).^2));
-    RCVy(ii) = rcov_run(idx,aa);
-end
-
-% WITH OVERNIGHT
-%%%%%%%%%%%%%%%%
-spy.Permno(:,1) = 84398;
-spy             = addOvernightRet(spy);
-aa              = addOvernightRet(aa);
-
-rv   = @(subs) sqrt(accumarray(subs,spy.Ret.^2,[],@nansum));
-rcov = @(subs,stock) accumarray(subs,spy.Ret.*stock.Ret,[],@nansum);
-
-RVd_on  = rv(iday);
-RCVd_on = rcov(iday,aa);
-
-RVm_on  = rv(imonth);
-RCVm_on = rcov(imonth,aa);
-
-RVy_on   = NaN(size(RVm));
-RCVy_on  = NaN(size(RVm));
-rcov_run = @(idx,stock) nansum(spy.Ret(idx).*stock.Ret(idx));
-for ii = 12:size(RVm,1)
-    idx         = ismember(imonth, ii-11:ii);
-    RVy_on(ii)  = sqrt(nansum(spy.Ret(idx).^2));
-    RCVy_on(ii) = rcov_run(idx,aa);
-end
-
-dt_daily   = yyyymmdd2datetime(Date);
-dt_monthly = serial2datetime(spy.Datetime(pos));
-
-% Plot RV
-figure
-set(gcf, 'Position', get(gcf,'Position').*[1,1,1,0.62],'PaperPositionMode','auto')
-plot(dt_daily, RVd,    dt_monthly, RVm,    dt_monthly, RVy,...
-     dt_daily, RVd_on, dt_monthly, RVm_on, dt_monthly, RVy_on)
-legend('daily','monthly','yearly','d_on','m_on','y_on','Location','NorthWest')
-legend boxoff
-set(gca,'Ytick',[0,0.1,0.2,0.3,0.4],'YtickLabel',{'0','10\%','20\%','30\%','40\%'})
-set(gca,'TickLabelInterpreter','latex')
-print('SPYrv','-depsc','-r200')
-
-% Plot RCV
-figure
-set(gcf, 'Position', get(gcf,'Position').*[1,1,1,0.62],'PaperPositionMode','auto')
-plot(dt_daily, RCVd,    dt_monthly, RCVm,    dt_monthly, RCVy,...
-     dt_daily, RCVd_on, dt_monthly, RCVm_on, dt_monthly, RCVy_on)
-legend('daily','monthly','yearly','d_on','m_on','y_on','Location','NorthWest')
-legend boxoff
-set(gca,'Ytick',[0,0.1,0.2,0.3],'YtickLabel',{'0','10\%','20\%','30\%'},'Ylim',[-0.01,0.3])
-set(gca,'TickLabelInterpreter','latex')
-
-% Plot RBETA
-figure
-set(gcf, 'Position', get(gcf,'Position').*[1,1,1,0.62],'PaperPositionMode','auto')
-plot(dt_daily, RCVd./RVd.^2,    dt_monthly, RCVm./RVm.^2,    dt_monthly, RCVy./RVy.^2,...
-     dt_daily, RCVd_on./RVd_on.^2, dt_monthly, RCVm_on./RVm_on.^2, dt_monthly, RCVy_on./RVy_on.^2)
-legend('daily','monthly','yearly','d_on','m_on','y_on','Location','NorthWest')
-legend boxoff
-set(gca,'Ytick',[0,0.1,0.2,0.3],'YtickLabel',{'0','10\%','20\%','30\%'},'Ylim',[-0.01,0.3])
-set(gca,'TickLabelInterpreter','latex')
